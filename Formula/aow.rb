03396ec52d1fc8884f8b885b5b9196fb6f916796
Kaustubh Patange <developerkp16@gmail.com>
2020-11-16 23:05:31 +0530

    [Update] v0.1.2

--- Formula/aow.rb
+++ Formula/aow.rb
@@ -1,9 +1,9 @@
 class aow < Formula
     desc "A command-line tool for adb to connect devices over wifi." 
     homepage "https://github.com/KaustubhPatange/aow"
-    url "https://github.com/KaustubhPatange/aow/releases/download/v0.1.1/aow-mac.tar.gz"
-    sha256 "0259a88e46c477f83c3f51678e55ee6198302c9b11d08f8c16f6b4d19acb7b3f"
-    version "0.1.1"
+    url "https://github.com/KaustubhPatange/aow/releases/download/v0.1.2/aow-mac.tar.gz"
+    sha256 "6df57bcda931569c813daa0dbf5d94016b2017de6faa694ad92b2bc1696a6192"
+    version "0.1.2"
   
     def install
       bin.install "aow"
